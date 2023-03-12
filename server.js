0b7c040dc5faf0a3a0a5ed882acd86a4e6957877
Anthony Keith <jt1keith@gmail.com>
2023-03-12 10:02:17 -0700

    connected back end to front end

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 console.log('server');
 console.log('testing')
 const { response, request } = require('express');
+const cors = require('cors');
 // REQUIRE
 // use require instead of import
 
@@ -14,10 +15,13 @@ require('dotenv').config();
 
 
 let data = require('./data/weather.json')
+
+// we must include cors if we want to share data.
 //USE
 // Assign the file a required variable (similar to reacts import)
 // Express Takes Two Steps
 const app = express( );
+app.use(cors())
 
 // define a PORT & validate env is working
 const PORT = process.env.PORT || 3002
@@ -27,11 +31,40 @@ const PORT = process.env.PORT || 3002
 // ROUTES 
 // we will use these to access our endpoints
 
-// define our default route
+
 // the first argument is a URL in quotes
 // the second is the callback that defines what should happen  when a request comes into that url
-app.get('/', (req, res) => {
-  res.send('Hello From Our Server')
+
+// example request: http:localhost:3001?city={this.state.userCityChoice}
+app.get('/?', (req, res, next) => {
+  try {
+    let userCity = req.query.city;
+    let forecastObject = data.find(obj => obj.city_name === userCity);
+    let forecastObjectData = forecastObject.data
+    let cityObject = forecastObjectData.map(obj => 
+      new Forecast(obj))
+    res.send(cityObject);
+
+    //res.send(selectedForecast);
+ } catch(error) {
+    next(error)
+  }
+  // res.send(data)
+});
+
+app.get('/movie', (req, res, next) => {
+  try {
+    let userCity = req.query.movieSearch;
+    console.log(userCity)
+    res.send(userCity)
+    
+    // res.send(cityObject);
+
+    //res.send(selectedForecast);
+ } catch(error) {
+    next(error)
+  }
+  // res.send(data)
 });
 
 app.get('*', (req, res) => {
@@ -40,6 +73,13 @@ app.get('*', (req, res) => {
 
 // CLASSES (PASS IN AN OBJECT)
 // Create a class that will only hold filtered data
+
+class Forecast{
+  constructor(cityObject){
+    this.date = cityObject.datetime;
+    this.description = cityObject.weather.description;
+  }
+}
 
 // Error Handler
 app.use((error, request, response, next) => {
